4bf7f73c0f171fdebe99c1c20cda48872ba2e4ac
Hugo Ferraz <hugofferraz@gmail.com>
2026-08-12 22:34:45 -0300

    Add files via upload

---
+++ aula-12 de agosto de 2026/C02EX11.cpp
@@ -0,0 +1,21 @@
+//C02EX11.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	bool x;
+	
+	x = true;
+	cout << "Valor X como Verdadeiro ...: " << x << endl;
+	
+	x = false;
+	cout << "Valor X como False ........: " << x << endl;
+	
+	cout << endl;
+	cout << "Tecle <Enter> para encerrar ... ";
+	cin.get();
+	return 0;
+}
